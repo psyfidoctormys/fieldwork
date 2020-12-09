46160859a308c46cd6fe2c1a2fe94fa1c3c1d48c
Elana Ying Mystal <elana.mystal@gmail.com>
2020-12-09 10:52:33 -0500

    nav bar and PostsController

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+        <div class="jumbotron text-center">
         <h1>{{$title}}</h1>
         <p>This is the Fieldwork Project to create a dashboard for sample management between Redcap and LabVantage</p>
-@endsection
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register</a></p>
+        </div>
+        @endsection
